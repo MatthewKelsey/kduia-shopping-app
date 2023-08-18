eaf37f2d312e5eef74a82e2e4cb0386e35abddd5
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-08-18 10:33:45 +0100

    This course is rubbish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -9,6 +9,12 @@ import Location from './components/Location';
 function App() {
   return (
     <div className="App">
+      <CartValue />
+      <ExpenseList />
+
+      <ItemSelected />
+      <Location />
+      
     </div>
   );
 }
